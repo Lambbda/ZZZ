ba5a0bfbb17019ed4cf0ea468bad7b458fdec5f9
Lambbda <nicklambda@yandex.ru>
2019-09-15 16:19:13 +0300

    testing phase 1

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -11,12 +11,12 @@ public class MainActivity extends AppCompatActivity {
     ArrayList<Date> end = new ArrayList<>();
     long current;   //time slept in current session
     String display;
-    void trimtimeline(int i){
-        Date yesterday = new Date(new Date().getTime() - 24 * 3600 * 1000l);
-        if (end.get(i).getTime()<yesterday.getTime()) {
+    void trimtimeline(int i){   //checks if sleep episode is within 24 hours from now
+        Date yesterday = new Date(new Date().getTime() - 60 * 1000l);//24 * 3600
+        if (end.get(i).getTime()<yesterday.getTime()) { //if sleep episode ended over 24 hours ago, discard it
             end.remove(i);
             start.remove(i);}
-            else if (start.get(i).getTime()<yesterday.getTime())
+            else if (start.get(i).getTime()<yesterday.getTime())    //only account the latest (24hr) part of sleep episode
                 start.set(i,yesterday);
     }
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,11 +33,11 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     end.add(java.util.Calendar.getInstance().getTime());
                     long total = 0;     //overall time slept
-                    for (int i=0;i<end.size();i++){
+                    for (int i=0;i<end.size();i++){ //sum all sleep episodes in past 24 hours
                         trimtimeline(i);
                         total+=(end.get(i).getTime()- start.get(i).getTime());
                     }
-                    display = "Slept "+total/1000*60*24+" hrs "+total/1000*60+" min";
+                    display = "Slept "+total/1000+" sec in past minute"; //" hrs "+total/1000*60+" min";
                     tv.setText(display);
                 }
             }
