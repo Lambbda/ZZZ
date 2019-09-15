194f1bc299f7711c98e5cb7e444bc29d44aa19d9
Lambbda <nicklambda@yandex.ru>
2019-09-15 15:03:40 +0300

    Potatoes

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -9,25 +9,9 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
     ArrayList<Date> start = new ArrayList<>();
     ArrayList<Date> end = new ArrayList<>();
-    boolean isChecked;
     long total;
     long current;
     String display;
-    Thread clock = new Thread(){
-        public void run(){
-            try {
-                while(isChecked){
-                    Date now = new Date();
-                    current = now.getTime()-start.get(start.size()).getTime();
-                    display = "Sleeping... "+current+" s";
-                    clock.sleep(1000);
-                }
-            } catch (InterruptedException e) {
-            Thread.currentThread().interrupt();
-            throw new RuntimeException("How the fuck did this get interrupted", e);
-        }
-        }
-    };
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
@@ -39,9 +23,7 @@ public class MainActivity extends AppCompatActivity {
                     start.add(java.util.Calendar.getInstance().getTime());
                     display = "Sleeping...";
                     tv.setText(display);
-                    clock.start();
                 } else {
-                    clock.stop();
                     end.add(java.util.Calendar.getInstance().getTime());
                     for (int i=0;i<end.size();i++){
                         total+=(end.get(i).getTime()- start.get(i).getTime());
