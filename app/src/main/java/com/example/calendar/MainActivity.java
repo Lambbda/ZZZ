75f53aab5118b7d5437182dbe21000fba477df15
Lambbda <nicklambda@yandex.ru>
2019-09-15 14:02:20 +0300

    Added arrays

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -3,24 +3,31 @@ package com.example.calendar;
 import android.os.Bundle;
 import android.widget.*;
 import java.util.Date;
+import java.util.ArrayList;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    Date t_push, t_release;
+    ArrayList<Date> start = new ArrayList<>();
+    ArrayList<Date> end = new ArrayList<>();
     long zzz;               //Difference between push and release in hours
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ToggleButton tbutton = findViewById(R.id.button);
-        tbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+        ToggleButton button = findViewById(R.id.button);
+        button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                TextView tv = findViewById(R.id.textView);
+                String display;
                 if (isChecked) {
-                    t_push=java.util.Calendar.getInstance().getTime();
+                    start.add(java.util.Calendar.getInstance().getTime());
+                    display = "Sleeping...";
+                    tv.setText(display);
                 } else {
-                    t_release=java.util.Calendar.getInstance().getTime();
-                    zzz = t_release.getTime() - t_push.getTime();
-                    TextView tv = findViewById(R.id.textView);
-                    String display = zzz/1000+" seconds";
+                    end.add(java.util.Calendar.getInstance().getTime());
+                    for (int i=0;i<end.size();i++){
+                        zzz+=(end.get(i).getTime()- start.get(i).getTime());
+                    }
+                    display = zzz/1000+" seconds";
                     tv.setText(display);
                 }
             }
