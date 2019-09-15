e944cec5f9019528b8c87a6e109c9c78d6dca03a
Lambbda <nicklambda@yandex.ru>
2019-09-15 12:51:38 +0300

    Added tb

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,15 +1,28 @@
 package com.example.calendar;
 
 import android.os.Bundle;
-
+import android.widget.*;
+import java.time.*;
+import java.util.Date;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+    Duration zzz;
+    Date t_push, t_release;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ToggleButton tbutton = (ToggleButton) findViewById(R.id.tbutton);
+        tbutton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    t_push=java.util.Calendar.getInstance().getTime();
+                } else {
+                    t_release=java.util.Calendar.getInstance().getTime();
+                    
+                }
+            }
+        });
         }
     }
 
